5410d76f3e0b608ed1246b21c162d821e1616998
i100van <i100van@hotmail.com>
2020-02-11 09:23:32 +0100

    Atrib. y constructor

--- src/main/java/com/example/padelversus/Match.java
+++ src/main/java/com/example/padelversus/Match.java
@@ -1,4 +1,28 @@
 package com.example.padelversus;
 
+import sun.util.resources.Bundles;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Team team_1;
+    private Team team_2;
+    private String score;
+    private String date;
+    private Team winner;
+
+    public Match() {
+    }
+    public Match(Team team_1,Team team_2,String score, String Date, Team winner){
+
+    }
 }
