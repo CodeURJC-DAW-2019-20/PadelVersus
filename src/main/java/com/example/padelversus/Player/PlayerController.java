2741084488ab5385c32d434c9b918803fdc6c436
Lucas Gómez Torres <45000089+LucasGomezTorres@users.noreply.github.com>
2020-02-11 11:10:09 +0100

    deletePlayerController2

--- src/main/java/com/example/padelversus/Player/PlayerController.java
+++ src/main/java/com/example/padelversus/Player/PlayerController.java
@@ -1,20 +1,44 @@
 package com.example.padelversus.Player;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/player")
 
 public class PlayerController {
 
+    @Autowired
+    PlayerRepository playerRepository;
+
+
     @GetMapping("/")
     public String player(Model model){
         return "player";
     }
+
+
+
+    @GetMapping("/{id}")
+    public String player(Model model, @PathVariable Long id){
+        Optional<Player> player = playerRepository.findById(id);
+        if (player.isPresent()) {
+            model.addAttribute("name",player.get().getUsername());
+            return "player";
+        } else {
+            return "404";
+        }
+    }
+
+
+
 
 
 
