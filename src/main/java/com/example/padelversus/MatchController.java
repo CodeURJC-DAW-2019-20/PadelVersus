a9c037ce1b3d6622d01bb65c1545efc0ccbddc46
i100van <i100van@hotmail.com>
2020-02-11 09:28:45 +0100

    Create match repository

--- src/main/java/com/example/padelversus/MatchController.java
+++ src/main/java/com/example/padelversus/MatchController.java
@@ -1,4 +1,12 @@
 package com.example.padelversus;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/match")
 public class MatchController {
+    @Autowired
+    Matchrepository
 }
---
+++ src/main/java/com/example/padelversus/MatchRepository.java
@@ -0,0 +1,4 @@
+package com.example.padelversus;
+
+public class MatchRepository {
+}
