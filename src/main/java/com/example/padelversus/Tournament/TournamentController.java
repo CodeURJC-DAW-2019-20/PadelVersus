bfe54114548cbd39c44b1a0e1fb809792c5f6a11
Jose <jllavado0@gmail.com>
2020-02-11 10:27:15 +0100

    Autowired repository

--- src/main/java/com/example/padelversus/Tournament/TournamentController.java
+++ src/main/java/com/example/padelversus/Tournament/TournamentController.java
@@ -1,16 +1,16 @@
 package com.example.padelversus.Tournament;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TournamentController {
 
-    //@Autowired
-    //TournamentRepository tournamentRepository;
+    @Autowired
+    TournamentRepository tournamentRepository;
 
     @GetMapping("/tournaments")
     public String loadTournament(Model model){
