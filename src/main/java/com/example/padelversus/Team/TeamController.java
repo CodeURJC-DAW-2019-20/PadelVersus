103ee39565537d5396f77d76b7883bf67a1cbe00
unknown <d.carmonap97@gmail.com>
2020-02-11 10:25:16 +0100

    TeamController is now TeamsController and can load the teams.html page

--- src/main/java/com/example/padelversus/Team/TeamController.java
+++
@@ -1,4 +0,0 @@
-package com.example.padelversus.Team;
-
-public class TeamController {
-}
---
+++ src/main/java/com/example/padelversus/Team/TeamsController.java
@@ -0,0 +1,22 @@
+package com.example.padelversus.Team;
+
+
+import com.example.padelversus.Player;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/teams")
+public class TeamsController {
+
+
+    @GetMapping("/")
+    public String teams(){
+        return "teams";
+    }
+}
