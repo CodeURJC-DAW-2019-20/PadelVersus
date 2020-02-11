f3515fa0a2e37a33788823f7c960f6e046c630e9
unknown <d.carmonap97@gmail.com>
2020-02-11 10:47:16 +0100

    In TeamRepository interface findbyid

--- src/main/java/com/example/padelversus/Team/TeamRepository.java
+++ src/main/java/com/example/padelversus/Team/TeamRepository.java
@@ -1,7 +1,12 @@
 package com.example.padelversus.Team;
 
+import com.example.padelversus.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-
+        public Optional<Team> findByid(Long id);
 }
