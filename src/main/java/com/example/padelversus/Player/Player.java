1c5ef72ffd525a82ed3ee192af6e7d96f1fe19c8
Lucas Gómez Torres <45000089+LucasGomezTorres@users.noreply.github.com>
2020-02-11 09:47:42 +0100

    PlayerEntityGettersSetters

--- src/main/java/com/example/padelversus/Player/Player.java
+++ src/main/java/com/example/padelversus/Player/Player.java
@@ -38,5 +38,91 @@ public class Player {
         this.imagen = imagen;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public double getFuerza() {
+        return fuerza;
+    }
+
+    public Byte getImagen() {
+        return imagen;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void setFuerza(double fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public void setImagen(Byte imagen) {
+        this.imagen = imagen;
+    }
 }
--- src/main/java/com/example/padelversus/Player/PlayerController.java
+++ src/main/java/com/example/padelversus/Player/PlayerController.java
@@ -2,12 +2,14 @@ package com.example.padelversus.Player;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/player")
 
 public class PlayerController {
+
 
 
 }
