4d6ca52f8a7792af522968d669e98648a4823c9f
i100van <i100van@hotmail.com>
2020-02-11 09:33:35 +0100

    Request mapping

--- src/main/java/com/example/padelversus/MatchRepository.java
+++ src/main/java/com/example/padelversus/MatchRepository.java
@@ -1,4 +1,11 @@
 package com.example.padelversus;
 
-public class MatchRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Team, Long> {
+     Optional<Team> findById(long id);
 }
