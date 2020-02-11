7e839bcee9055b2b4110b0d5818b5d811d5b9540
Jose <jllavado0@gmail.com>
2020-02-11 09:51:34 +0100

    Addconstructor and getters and setters

--- src/main/java/com/example/padelversus/Tournament/Tournament.java
+++ src/main/java/com/example/padelversus/Tournament/Tournament.java
@@ -18,4 +18,54 @@ public class Tournament {
 
     //private List<Match> Matches;
     //private Team winner;
+
+    public Tournament() {
+    }
+
+    /*public Tournament(String name, String date, List<Match> matches, Team winner) {
+        this.name = name;
+        this.date = date;
+        Matches = matches;
+        this.winner = winner;
+    }*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /*public List<Match> getMatches() {
+        return Matches;
+    }
+
+    public void setMatches(List<Match> matches) {
+        Matches = matches;
+    }
+
+    public Team getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Team winner) {
+        this.winner = winner;
+    }*/
 }
