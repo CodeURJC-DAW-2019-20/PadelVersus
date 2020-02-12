0525b9b0123ecbf8d2d805428200c8bb1786a22e
lujoselu98 <jllavado0@gmail.com>
2020-02-12 21:13:07 +0100

    Add more example data, better explained

--- src/main/java/com/example/padelversus/PadelversusApplication.java
+++ src/main/java/com/example/padelversus/PadelversusApplication.java
@@ -48,11 +48,32 @@ public class PadelversusApplication implements ApplicationRunner {
         Player player3 = new Player("Jose","joseluisls",29,1.89,50,"arena","CaptianTeam",5,10,"Netherland");
         Player player4 = new Player("Dani","danicp",20,1.40,59,"sombrilla","RayoTeam",4,7,"Sweden");
         Player player5 = new Player("Ivan","ivanms",10,1.59,67,"alfombra","RelampagoTeam",6,9,"United Kingdom");
-        Player player6 = new Player("Ivan2","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player6 = new Player("Juan","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player7 = new Player("Pedro","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player8 = new Player("Luis","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player9 = new Player("Mario","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player10 = new Player("Guillermo","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player11 = new Player("Luis","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player12 = new Player("Israel","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player13 = new Player("Maria","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player14 = new Player("Pilar","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player15 = new Player("Alejandra","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player16 = new Player("Carmen","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player17 = new Player("Nerea","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player18 = new Player("Daniel","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player19 = new Player("Manuel","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
+        Player player20 = new Player("Ana","ivanms",102,1.592,672,"alfombra2","RelampagoTeam2",62,92,"United Kingdom2");
 
-        Team t1 =new Team("Danielos",player1,player4,new TeamStatistics(10,40));
-        Team t2 =new Team("Joselos",player2,player3,new TeamStatistics(15,45));
-        Team t3 =new Team("Papateros",player5,player5,new TeamStatistics(20,42));
+        Team t1 =new Team("Danielos",player1,player2,new TeamStatistics(10,40));
+        Team t2 =new Team("Joselos",player3,player4,new TeamStatistics(15,45));
+        Team t3 =new Team("Papateros",player5,player6,new TeamStatistics(20,42));
+        Team t4 =new Team("Leones",player7,player8,new TeamStatistics(20,42));
+        Team t5 =new Team("Tigres",player9,player10,new TeamStatistics(20,42));
+        Team t6 =new Team("Donuts",player11,player12,new TeamStatistics(20,42));
+        Team t7 =new Team("PapdelTeam",player13,player14,new TeamStatistics(20,42));
+        Team t8 =new Team("VersusTeam",player15,player16,new TeamStatistics(20,42));
+        Team t9 =new Team("Luqueros",player17,player18,new TeamStatistics(20,42));
+        Team t10 =new Team("Jamoneros",player18,player20,new TeamStatistics(20,42));
 
         playerRepository.save(player1);
         playerRepository.save(player2);
@@ -60,25 +81,81 @@ public class PadelversusApplication implements ApplicationRunner {
         playerRepository.save(player4);
         playerRepository.save(player5);
         playerRepository.save(player6);
+        playerRepository.save(player7);
+        playerRepository.save(player8);
+        playerRepository.save(player9);
+        playerRepository.save(player10);
+        playerRepository.save(player11);
+        playerRepository.save(player12);
+        playerRepository.save(player13);
+        playerRepository.save(player14);
+        playerRepository.save(player15);
+        playerRepository.save(player16);
+        playerRepository.save(player17);
+        playerRepository.save(player18);
+        playerRepository.save(player19);
+        playerRepository.save(player19);
+        playerRepository.save(player20);
 
         teamRepository.save(t1);
         teamRepository.save(t2);
         teamRepository.save(t3);
+        teamRepository.save(t4);
+        teamRepository.save(t5);
+        teamRepository.save(t6);
+        teamRepository.save(t7);
+        teamRepository.save(t8);
+        teamRepository.save(t9);
+        teamRepository.save(t10);
+
+
+
 
         MatchStadistics ms1_1 = new MatchStadistics(23,57,50,50,true);
         MatchStadistics ms1_2 = new MatchStadistics(13,17,0,0, false);
+        Match match1 = new Match("t1 vs t2 win t2","20/20/2020",ms1_1,ms1_2,t1,t2);
+
         MatchStadistics ms2_1 = new MatchStadistics(99,99,99,99,true);
         MatchStadistics ms2_2 = new MatchStadistics(78,7,9,3,false);
+        Match match2 = new Match("t3 vs t1 win t3","21/20/2020",ms2_1,ms2_2,t3,t1);
+
         MatchStadistics ms3_1 = new MatchStadistics(23,57,50,50,false);
         MatchStadistics ms3_2 = new MatchStadistics(23,57,50,50,true);
+        Match match3 = new Match("t2 vs t3 win t3","22/20/2020",ms3_1,ms3_2,t2,t3);
 
-        Match match1 = new Match("patata","20/20/2020",ms1_1,ms1_2,t1,t2);
-        Match match2 = new Match("patata","21/20/2020",ms2_1,ms2_2,t3,t1);
-        Match match3 = new Match("patata","22/20/2020",ms3_1,ms3_2,t2,t3);
+        MatchStadistics ms4_1 = new MatchStadistics(23,57,50,50,true);
+        MatchStadistics ms4_2 = new MatchStadistics(23,57,50,50,false);
+        Match match4 = new Match("t4 vs t5 win t4","22/20/2020",ms4_1,ms4_2,t4,t5);
+
+        MatchStadistics ms5_1 = new MatchStadistics(23,57,50,50,false);
+        MatchStadistics ms5_2 = new MatchStadistics(23,57,50,50,true);
+        Match match5 = new Match("t6 vs t7 win t7","22/20/2020",ms5_1,ms5_2,t6,t7);
+
+        MatchStadistics ms6_1 = new MatchStadistics(23,57,50,50,false);
+        MatchStadistics ms6_2 = new MatchStadistics(23,57,50,50,true);
+        Match match6 = new Match("t4 vs t7 wins t7","22/20/2020",ms6_1,ms6_2,t4,t7);
+
+        MatchStadistics ms7_1 = new MatchStadistics(23,57,50,50,false);
+        MatchStadistics ms7_2 = new MatchStadistics(23,57,50,50,true);
+        Match match7 = new Match("t8 vs t10 wins t10","22/20/2020",ms7_1,ms7_2,t8,t10);
+
+        MatchStadistics ms8_1 = new MatchStadistics(23,57,50,50,false);
+        MatchStadistics ms8_2 = new MatchStadistics(23,57,50,50,true);
+        Match match8 = new Match("t8 vs t9 wins t9","22/20/2020",ms8_1,ms8_2,t8,t9);
+
+        MatchStadistics ms9_1 = new MatchStadistics(23,57,50,50,false);
+        MatchStadistics ms9_2 = new MatchStadistics(23,57,50,50,true);
+        Match match9 = new Match("t9 vs t10 wins t10","22/20/2020",ms9_1,ms9_2,t9,t10);
 
         matchRepository.save(match1);
         matchRepository.save(match2);
         matchRepository.save(match3);
+        matchRepository.save(match4);
+        matchRepository.save(match5);
+        matchRepository.save(match6);
+        matchRepository.save(match7);
+        matchRepository.save(match8);
+        matchRepository.save(match9);
 
         List<Match> tournamrnt1_matches =  new ArrayList<>();
         tournamrnt1_matches.add(match1);
@@ -86,7 +163,21 @@ public class PadelversusApplication implements ApplicationRunner {
         tournamrnt1_matches.add(match3);
         Tournament tournament1 = new Tournament("Tournament 1", tournamrnt1_matches);
 
+        List<Match> tournamrnt2_matches =  new ArrayList<>();
+        tournamrnt2_matches.add(match4);
+        tournamrnt2_matches.add(match5);
+        tournamrnt2_matches.add(match6);
+        Tournament tournament2 = new Tournament("Tournament 2", tournamrnt2_matches);
+
+        List<Match> tournamrnt3_matches =  new ArrayList<>();
+        tournamrnt3_matches.add(match7);
+        tournamrnt3_matches.add(match8);
+        tournamrnt3_matches.add(match9);
+        Tournament tournament3 = new Tournament("Tournament 3", tournamrnt3_matches);
+
         tournamentRepository.save(tournament1);
+        tournamentRepository.save(tournament2);
+        tournamentRepository.save(tournament3);
 
 
     }
