5600e836d2627d081d9fd7339a1293599fd06c5f
i100van <i100van@hotmail.com>
2020-02-12 13:11:08 +0100

    Change upper case

--- src/main/java/com/example/padelversus/team/TeamxController.java
+++ src/main/java/com/example/padelversus/team/TeamxController.java
@@ -1,4 +1,4 @@
-package com.example.padelversus.Team;
+package com.example.padelversus.team;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
