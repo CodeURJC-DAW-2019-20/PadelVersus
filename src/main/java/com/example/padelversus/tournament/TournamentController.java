aa1d09a1e5c5d9ba9df57b46d4ee2b4d58348719
lujoselu98 <jllavado0@gmail.com>
2020-02-13 21:14:13 +0100

    Logic to create display entities

--- src/main/java/com/example/padelversus/tournament/TournamentController.java
+++ src/main/java/com/example/padelversus/tournament/TournamentController.java
@@ -1,11 +1,15 @@
 package com.example.padelversus.tournament;
 
 
+import com.example.padelversus.team.Team;
+import com.example.padelversus.tournament.display.TeamDisplay;
+import com.example.padelversus.tournament.display.TournamentDisplay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,8 +23,23 @@ public class TournamentController {
     @GetMapping("/tournaments")
     public String loadTournament(Model model) {
         List<Tournament> allTournament = tournamentRepository.findAll();
+        List<TournamentDisplay> allTournamentDisplay = new ArrayList<>();
         for (Tournament tournament : allTournament) {
             tournamentService.teamOrder(tournament);
+            TournamentDisplay tournamentDisplay = new TournamentDisplay(tournament);
+            for(Team team: tournament.getTeams()){
+                int [] wonPlayed = tournamentService.wonGames(tournament, team);
+                tournamentDisplay.addTeam(team, wonPlayed[0], wonPlayed[1]);
+            }
+            allTournamentDisplay.add(tournamentDisplay);
+            /*
+            System.out.println(tournament.getName());
+            for (Team team : tournament.getTeams()) {
+                System.out.println("Team name: " + team.getName() +
+                        "\tWon: " + tournamentService.wonGames(tournament, team)[0] +
+                        "\tPlayed: "+ tournamentService.wonGames(tournament, team)[1]);
+            }
+            System.out.println();*/
         }
         /*System.out.println("Estoy en /tournament");
         Optional<Tournament> Optionaltournament = tournamentRepository.getById(1L);
@@ -40,7 +59,7 @@ public class TournamentController {
             tournament_names.add(tournament.getName());
         }
         System.out.println(tournament_names);*/
-        model.addAttribute("tournament-list", allTournament);
+        model.addAttribute("tournament-list", allTournamentDisplay);
         return "Tournaments";
     }
 
