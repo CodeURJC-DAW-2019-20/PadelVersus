72c548c0e9f6c8ee6383fc208ac3f304cdf7898f
lujoselu98 <jllavado0@gmail.com>
2020-02-13 20:48:34 +0100

    Controller imports

--- src/main/java/com/example/padelversus/tournament/TournamentController.java
+++ src/main/java/com/example/padelversus/tournament/TournamentController.java
@@ -1,15 +1,12 @@
 package com.example.padelversus.tournament;
 
 
-import com.example.padelversus.team.Team;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class TournamentController {
