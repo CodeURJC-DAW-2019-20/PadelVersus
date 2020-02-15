db6862907125e5d90918198c39906f47781eafe8
Jose <jllavado0@gmail.com>
2020-02-15 14:52:07 +0100

    Logic to join user and player on database

--- src/main/java/com/example/padelversus/user/UserController.java
+++ src/main/java/com/example/padelversus/user/UserController.java
@@ -4,59 +4,73 @@ import com.example.padelversus.mail.NotificationService;
 import com.example.padelversus.player.Player;
 import com.example.padelversus.player.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
 
     @Autowired
-    private UserRepository uc;
+    private UserRepository userRepository;
 
     @Autowired
-    private PlayerRepository pc;
+    private PlayerRepository playerRepository;
     @Autowired
     private NotificationService notificationService;
 
 
     @PostMapping("/saveUser")
-    public String saveUser(User user){
-        //user.setRol("ROLE_USER");
-        //User u = uc.save(user);
-        //return "/";
-        User u = uc.findByName(user.getName());
+    public String saveUser(User user, Model model){
+        User u = userRepository.findByName(user.getName());
         if (u == null) {
             User userSave = new User();
             userSave.setName(user.getName());
             userSave.setPasswordHash(user.getPasswordHash());
             userSave.setMail(user.getMail());
             userSave.setRol("USER_ROLE");
-            uc.save(userSave);
+            userRepository.save(userSave);
             notificationService.sendNotification(user);
-            //updateTabs(model);
-            return "/signupPlayer";
+            model.addAttribute("user_name", userSave.getName());
+            return "signupPlayer";
         }
 
         return "404";
     }
 
     @PostMapping("/signupPlayer")
-    public String signupPlayer(Player player){
-        pc.save(player);
-        return "/signupSuccess";
+    public String signupPlayer(Player player, @RequestParam String username){
+        User relatedUser = userRepository.findByName(username);
+        if(relatedUser != null) {
+            Player playerSave = new Player();
+
+            playerSave.setAge(player.getAge());
+            playerSave.setImage(player.getImage());
+            playerSave.setCountryBirth(player.getCountryBirth());
+            playerSave.setHeight(player.getHeight());
+            playerSave.setWeight(player.getWeight());
+            playerSave.setSpeed(player.getSpeed());
+            playerSave.setStrength(player.getStrength());
+            playerSave.setEndurance(player.getEndurance());
+            playerSave.setPace(player.getPace());
+            playerSave.setAccuaracy(player.getAccuaracy());
+            playerSave.setAceleration(player.getAceleration());
+
+            player.setUser(relatedUser);
+            playerRepository.save(player);
+            return "/signupSuccess";
+        }
+        return "404";
     }
 
     /*public String crear(@RequestParam String name, String pass, BindingResult bindingResult, ModelMap mp) {
-        uc.save(new User(name,pass,"ROLE_USER"));
+        userRepository.save(new User(name,pass,"ROLE_USER"));
         System.out.println("Crear");
         /*if (bindingResult.hasErrors()) {
             System.out.println("HAY ERROR");
             return "/crud/nuevo";
         } else {
-            uc.save(user);
+            userRepository.save(user);
             mp.put("usuario", user);
             return "/";
         }
