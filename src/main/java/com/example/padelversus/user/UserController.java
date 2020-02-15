6d4e92a6dd524eb10926126567c54a2f9c89439e
Jose <jllavado0@gmail.com>
2020-02-15 15:06:53 +0100

    Minor refactoring

--- src/main/java/com/example/padelversus/user/UserController.java
+++ src/main/java/com/example/padelversus/user/UserController.java
@@ -1,19 +1,17 @@
 package com.example.padelversus.user;
 
-import com.example.padelversus.mail.NotificationService;
 import com.example.padelversus.player.Player;
 import com.example.padelversus.player.PlayerRepository;
 import com.example.padelversus.player.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private UserService userService;
@@ -22,24 +20,22 @@ public class UserController {
     @Autowired
     private PlayerService playerService;
 
-    @Autowired
-    private PlayerRepository playerRepository;
 
 
     @PostMapping("/saveUser")
-    public String saveUser(User user, Model model){
+    public String saveUser(User user, Model model) {
         String userName = userService.saveUser(user);
-        if(userName != null){
+        if (userName != null) {
             model.addAttribute("user_name", userName);
             return "signupPlayer";
-        }else {
+        } else {
             return "404";
         }
     }
 
     @PostMapping("/signupPlayer")
-    public String signupPlayer(Player player, @RequestParam String username){
-        if(playerService.savePlayer(player, username)) {
+    public String signupPlayer(Player player, @RequestParam String username) {
+        if (playerService.savePlayer(player, username)) {
             return "/signupSuccess";
         }
         return "404";
