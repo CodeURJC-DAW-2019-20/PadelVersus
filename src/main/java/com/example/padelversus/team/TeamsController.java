73f66364c5e9bc2fe0e56cdef7a1833d816fc8c9
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-02-15 09:00:28 +0100

    All teams now show in the slider

--- src/main/java/com/example/padelversus/team/TeamsController.java
+++ src/main/java/com/example/padelversus/team/TeamsController.java
@@ -3,8 +3,12 @@ package com.example.padelversus.team;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/teams")
@@ -14,7 +18,15 @@ public class TeamsController {
     TeamRepository teamRepository;
 
     @GetMapping("")
-    public String teams(){
+    public String teams(Model model){
+        List<Team> allTeams = teamRepository.findAll();
+        List<String> teamNames = new ArrayList<>();
+
+        for(Team t: allTeams){
+            teamNames.add(t.getName());
+        }
+
+        model.addAttribute("teamNames", teamNames);
         return "teams";
     }
 }
