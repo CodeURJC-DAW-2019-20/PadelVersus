82d3956ee89584137e26c3ef09d6d0a2b7fc0103
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-02-15 18:04:52 +0100

    TeamStatisticsDisplay added

--- src/main/java/com/example/padelversus/team/display/TeamStatisticsDisplay.java
+++ src/main/java/com/example/padelversus/team/display/TeamStatisticsDisplay.java
@@ -1,11 +1,70 @@
 package com.example.padelversus.team.display;
 
+import com.example.padelversus.team.teamstatistics.TeamStatistics;
+
 public class TeamStatisticsDisplay {
     private String totalWins;
     private String totalDefeats;
-    private String totalAcurracy;
-    private String totalEffectiveness;
-    private String totalGamesWon;
-    private String totalUnforcedErrors;
+    private String meanAcurracy;
+    private String meanEffectiveness;
+    private String meanUnforcedErrors;
     private String gamesPerMatch;
+
+    public TeamStatisticsDisplay(TeamStatistics statistics){
+        String[] parsedStatistics = statistics.meanStatisticsParsed();
+        this.totalWins = parsedStatistics[0];
+        this.totalDefeats = parsedStatistics[1];
+        this.meanAcurracy = parsedStatistics[2];
+        this.meanEffectiveness = parsedStatistics[3];
+        this.meanUnforcedErrors = parsedStatistics[4];
+        this.gamesPerMatch= parsedStatistics[5];
+    }
+
+    public String getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(String totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    public String getTotalDefeats() {
+        return totalDefeats;
+    }
+
+    public void setTotalDefeats(String totalDefeats) {
+        this.totalDefeats = totalDefeats;
+    }
+
+    public String getMeanAcurracy() {
+        return meanAcurracy;
+    }
+
+    public void setMeanAcurracy(String meanAcurracy) {
+        this.meanAcurracy = meanAcurracy;
+    }
+
+    public String getMeanEffectiveness() {
+        return meanEffectiveness;
+    }
+
+    public void setMeanEffectiveness(String meanEffectiveness) {
+        this.meanEffectiveness = meanEffectiveness;
+    }
+
+    public String getMeanUnforcedErrors() {
+        return meanUnforcedErrors;
+    }
+
+    public void setMeanUnforcedErrors(String meanUnforcedErrors) {
+        this.meanUnforcedErrors = meanUnforcedErrors;
+    }
+
+    public String getGamesPerMatch() {
+        return gamesPerMatch;
+    }
+
+    public void setGamesPerMatch(String gamesPerMatch) {
+        this.gamesPerMatch = gamesPerMatch;
+    }
 }
