a6b2e49f394abed01dbab602a06a8885c14e5529
Jose <jllavado0@gmail.com>
2020-02-15 15:06:18 +0100

    Correct refactoring of saveplayer (joined to user) logic

--- src/main/java/com/example/padelversus/user/UserController.java
+++ src/main/java/com/example/padelversus/user/UserController.java
@@ -3,6 +3,7 @@ package com.example.padelversus.user;
 import com.example.padelversus.mail.NotificationService;
 import com.example.padelversus.player.Player;
 import com.example.padelversus.player.PlayerRepository;
+import com.example.padelversus.player.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+    @Autowired
+    private PlayerService playerService;
 
     @Autowired
     private PlayerRepository playerRepository;
@@ -34,42 +39,11 @@ public class UserController {
 
     @PostMapping("/signupPlayer")
     public String signupPlayer(Player player, @RequestParam String username){
-        User relatedUser = userRepository.findByName(username);
-        if(relatedUser != null) {
-            Player playerSave = new Player();
-
-            playerSave.setAge(player.getAge());
-            playerSave.setImage(player.getImage());
-            playerSave.setCountryBirth(player.getCountryBirth());
-            playerSave.setHeight(player.getHeight());
-            playerSave.setWeight(player.getWeight());
-            playerSave.setSpeed(player.getSpeed());
-            playerSave.setStrength(player.getStrength());
-            playerSave.setEndurance(player.getEndurance());
-            playerSave.setPace(player.getPace());
-            playerSave.setAccuaracy(player.getAccuaracy());
-            playerSave.setAceleration(player.getAceleration());
-
-            player.setUser(relatedUser);
-            playerRepository.save(player);
+        if(playerService.savePlayer(player, username)) {
             return "/signupSuccess";
         }
         return "404";
     }
-
-    /*public String crear(@RequestParam String name, String pass, BindingResult bindingResult, ModelMap mp) {
-        userRepository.save(new User(name,pass,"ROLE_USER"));
-        System.out.println("Crear");
-        /*if (bindingResult.hasErrors()) {
-            System.out.println("HAY ERROR");
-            return "/crud/nuevo";
-        } else {
-            userRepository.save(user);
-            mp.put("usuario", user);
-            return "/";
-        }
-        return "/";
-    }*/
 
 
 }
