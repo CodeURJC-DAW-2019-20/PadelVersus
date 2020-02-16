95d8ace73401a0ab5ebfc4232c53e93ee73541ae
AlexCh98 <alexcheca.98@gmail.com>
2020-02-16 11:29:07 +0100

    Matches results adding by admin, working on it

--- src/main/java/com/example/padelversus/admin/AdminController.java
+++ src/main/java/com/example/padelversus/admin/AdminController.java
@@ -34,6 +34,12 @@ public class AdminController {
     @RequestMapping("/adminPage")
     public String adminPage(Model model){
         List<Tournament> allTournament = tournamentRepository.findAll();
+        List<Match> allMatches = matchRepository.findAll();
+        for (int i = 0; i < allMatches.size(); i++) {
+            if(allMatches.get(i).getScore()!= null){
+                allMatches.remove(i);
+            }
+        }
         List<TournamentDisplay> allTournamentDisplay = new ArrayList<>();
         for (Tournament tournament : allTournament) {
             //tournamentService.teamOrder(tournament);
@@ -44,7 +50,8 @@ public class AdminController {
         }
         model.addAttribute("tournament-list", allTournament);
         //model.addAttribute("tournament-teams",tournament.getTeams());
-
+        //List<Match> allMatches = matchRepository.findAll();
+        model.addAttribute("match-list",allMatches);
         return "/adminPage";
     }
     @RequestMapping("/prueba")
@@ -76,6 +83,41 @@ public class AdminController {
         //List<Match> matches = (List<Match>) match;
        // tournament.setMatches(matches);
       //  tournamentRepository.save(tournament);
+        //matchRepository.save(match);
+        /*User u = uc.findByName(user.getName());
+        if (u == null) {
+            uc.save(user);
+            //notificationService.sendNotification(user);
+            //updateTabs(model);
+            return "/signupPlayer";
+        }
+
+       // return "404";*/
+        return "/adminPage";
+    }
+    @PostMapping("/saveDataMatch")
+    public String saveDatamatch() {
+        /*System.out.println(torneoSeleccionado);
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println("MATCH DATE:"+date);
+        Optional<Team> team = teamRepository.findByName(t1);
+        Optional<Team> team2 = teamRepository.findByName(t2);
+        String[] parts = date.split("-");
+
+        Match match = new Match(new Date(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]) , Integer.parseInt(parts[2])), team.get().getTeam(),team2.get().getTeam());
+        matchRepository.save(match);
+        Optional<Tournament> tournament = tournamentRepository.findByName(torneoSeleccionado);
+        List<Match> matches = new ArrayList<>();
+        matches.add(match);
+        tournament.get().setMatches(matches);
+        tournamentRepository.save(tournament.get()); //save the new match in tournament table
+
+        System.out.println(team.toString());
+        //Match match1 = new Match(date,t1,t2);
+        //List<Match> matches = (List<Match>) match;
+        // tournament.setMatches(matches);
+        //  tournamentRepository.save(tournament);
         //matchRepository.save(match);
         /*User u = uc.findByName(user.getName());
         if (u == null) {
