d49d086a94f8c5c68fb80e78fa76b756d1b3a726
Jose <jllavado0@gmail.com>
2020-02-16 16:26:14 +0100

    new template for uploadImage

--- src/main/java/com/example/padelversus/user/UserController.java
+++ src/main/java/com/example/padelversus/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UserController {
