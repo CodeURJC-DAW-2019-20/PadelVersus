ebb38fada33539d8eedc7d311b79304ba27a9eae
i100van <i100van@hotmail.com>
2020-02-17 17:07:25 +0100

    Teams names in overview matches

--- src/main/java/com/example/padelversus/match/MatchController.java
+++ src/main/java/com/example/padelversus/match/MatchController.java
@@ -1,5 +1,6 @@
 package com.example.padelversus.match;
 
+import com.example.padelversus.match.Stadistics.MatchStadistics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
@@ -15,12 +17,16 @@ public class MatchController {
     @Autowired
     MatchRepository matchRepository;
 
+
     @GetMapping("/{id}")
     public String match(Model model, @PathVariable Long id){
         Optional<Match> match = matchRepository.findById(id);
         if (match.isPresent()) {
-            //model.addAttribute("team_1", match.get().getTeam_1());
-            //model.addAttribute("team_2", match.get().getTeam_2());
+            ArrayList teams= (ArrayList) match.get().getTeams();
+            MatchStadistics m1 = match.get().getStadistics_1();
+            MatchStadistics m2 = match.get().getStadistics_2();
+            model.addAttribute("team_1", teams.get(0));
+            model.addAttribute("team_2", teams.get(0));
             model.addAttribute("id",id);
             model.addAttribute("accuracy_1",match.get().getStadistics_1().getAcurracy());
             model.addAttribute("accuracy_2",match.get().getStadistics_2().getAcurracy());
@@ -31,7 +37,6 @@ public class MatchController {
             model.addAttribute("unferrors_1",match.get().getStadistics_1().getUnforcedErrors());
             model.addAttribute("unferrors_2",match.get().getStadistics_1().getUnforcedErrors());
             model.addAttribute("score", match.get().getScore());
-            //model.addAttribute("winner", match.get().getWinner());
             model.addAttribute("date", match.get().getDate());
             return "overviewMatches";
         } else {
