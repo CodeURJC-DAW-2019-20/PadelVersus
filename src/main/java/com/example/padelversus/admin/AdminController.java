01c58f9118521887c8b116deef9f0d9a43a9a625
AlexCh98 <alexcheca.98@gmail.com>
2020-02-18 10:09:40 +0100

    Solve Problems

--- src/main/java/com/example/padelversus/admin/AdminController.java
+++ src/main/java/com/example/padelversus/admin/AdminController.java
@@ -36,33 +36,29 @@ public class AdminController {
     @Autowired
     TournamentService tournamentService;
     @Autowired
-    TeamRepository  teamRepository;
+    TeamRepository teamRepository;
     @Autowired
     SetPadelRepository setPadelRepository;
     @Autowired
     MatchStadisticsRepository matchStadisticsRepository;
+
     @RequestMapping("/adminPage")
-    public String adminPage(Model model){
+    public String adminPage(Model model) {
         List<Tournament> allTournament = tournamentRepository.findAll();
         List<Match> allMatches = matchRepository.findAll();
         List<MatchAdmin> matchAdmins = new ArrayList<>();
-       // System.out.println(allMatches.toString());
 
         List<TournamentDisplay> allTournamentDisplay = new ArrayList<>();
         for (Tournament tournament : allTournament) {
-            //tournamentService.teamOrder(tournament);
-            //TournamentDisplay tournamentDisplay = new TournamentDisplay(tournament);
-            //model.addAttribute("tournament-teams-"+tournament.getName(),tournament.getTeams());
-            for (Match match: tournament.getMatches()) {
+            for (Match match : tournament.getMatches()) {
                 List<Team> teamList = match.getTeams();
-                if(!match.isPlayed()){
-                    MatchAdmin matchAdmin = new MatchAdmin(teamList.get(0),teamList.get(1),match.getDate(),tournament);
+                if (!match.isPlayed()) {
+                    MatchAdmin matchAdmin = new MatchAdmin(teamList.get(0), teamList.get(1), match.getDate(), tournament);
                     matchAdmins.add(matchAdmin);
                 }
             }
-            //tournamentService.teamOrder(tournament);
             TournamentDisplay tournamentDisplay = new TournamentDisplay(tournament);
-            for(Team team: tournament.getTeams()){
+            for (Team team : tournament.getTeams()) {
                 List<String> lastMatches = tournamentService.lastThreeMatches(tournament, team);
                 tournamentDisplay.addTeam(team, 0, 0, lastMatches);
             }
@@ -71,83 +67,58 @@ public class AdminController {
         }
 
         model.addAttribute("tournament-list", allTournamentDisplay);
-        //model.addAttribute("tournament-teams",tournament.getTeams());
-        //List<Match> allMatches = matchRepository.findAll();
-        model.addAttribute("match-list",matchAdmins);
-
+        model.addAttribute("match-list", matchAdmins);
 
         return "/adminPage";
     }
+
     @RequestMapping("/prueba")
-    public String prueba(){
+    public String prueba() {
 
         return "/prueba";
     }
 
     @PostMapping("/saveMatch")
-    public String savematch(String torneoSeleccionado,String t1_oficial, String t2_oficial, String date) {
+    public String savematch(String torneoSeleccionado, String t1_oficial, String t2_oficial, String date) {
         System.out.println(torneoSeleccionado);
         System.out.println(t1_oficial);
         System.out.println(t2_oficial);
-        System.out.println("MATCH DATE:"+date);
+        System.out.println("MATCH DATE:" + date);
         Optional<Tournament> tournament = tournamentRepository.findByName(torneoSeleccionado);
         String[] teamName1 = t1_oficial.split(",");
         String[] teamName2 = t2_oficial.split(",");
         Team team1 = new Team();
         for (String s : teamName1) {
             Optional<Team> teamaux = teamRepository.findByName(s);
-            if(teamaux.isPresent()){
-                //System.out.println(teamaux.get().getName());
-                //System.out.println(tournament.get().hasTeam(teamaux.get()));
+            if (teamaux.isPresent()) {
                 if (tournament.get().hasTeam(teamaux.get())) {
                     team1 = teamaux.get();
-                    //System.out.println(teamaux.get().getName());
                 }
             }
         }
         Team team2 = new Team();
         for (String s : teamName2) {
             Optional<Team> teamaux = teamRepository.findByName(s);
-            if(teamaux.isPresent()){
-                //System.out.println(teamaux.get().getName());
-                //System.out.println(tournament.get().hasTeam(teamaux.get()));
+            if (teamaux.isPresent()) {
                 if (tournament.get().hasTeam(teamaux.get())) {
                     team2 = teamaux.get();
-                    //System.out.println(teamaux.get().getName());
                 }
             }
         }
         String[] parts = date.split("-");
 
-
-        Match match = new Match(false, LocalDate.of(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]) , Integer.parseInt(parts[2])), team1,team2);
+        Match match = new Match(false, LocalDate.of(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])), team1, team2);
 
         matchRepository.save(match);
         team1.addMatch(match);
         team2.addMatch(match);
         teamRepository.save(team1);
         teamRepository.save(team2);
-        //Optional<Tournament> tournament = tournamentRepository.findByName(torneoSeleccionado);
         List<Match> matches = new ArrayList<>();
         matches.add(match);
         tournament.get().setMatches(matches);
         tournamentRepository.save(tournament.get()); //save the new match in tournament table
 
-        //System.out.println(team.toString());
-        //Match match1 = new Match(date,t1,t2);
-        //List<Match> matches = (List<Match>) match;
-       // tournament.setMatches(matches);
-      //  tournamentRepository.save(tournament);
-        //matchRepository.save(match);
-        /*User u = uc.findByName(user.getName());
-        if (u == null) {
-            uc.save(user);
-            //notificationService.sendNotification(user);
-            //updateTabs(model);
-            return "/signupPlayer";
-        }
-
-       // return "404";*/
         return "/adminPage";
     }
 
@@ -155,81 +126,21 @@ public class AdminController {
     public String savetournament(String name) {
         Tournament tournament = new Tournament(name);
         System.out.println(tournament.toString());
-        /*Optional<Tournament> tournament = tournamentRepository.findByName(torneoSeleccionado);
-        String[] teamName1 = t1_oficial.split(",");
-        String[] teamName2 = t2_oficial.split(",");
-        Team team1 = new Team();
-        for (String s : teamName1) {
-            Optional<Team> teamaux = teamRepository.findByName(s);
-            if(teamaux.isPresent()){
-                //System.out.println(teamaux.get().getName());
-                //System.out.println(tournament.get().hasTeam(teamaux.get()));
-                if (tournament.get().hasTeam(teamaux.get())) {
-                    team1 = teamaux.get();
-                    //System.out.println(teamaux.get().getName());
-                }
-            }
-        }
-        Team team2 = new Team();
-        for (String s : teamName2) {
-            Optional<Team> teamaux = teamRepository.findByName(s);
-            if(teamaux.isPresent()){
-                //System.out.println(teamaux.get().getName());
-                //System.out.println(tournament.get().hasTeam(teamaux.get()));
-                if (tournament.get().hasTeam(teamaux.get())) {
-                    team2 = teamaux.get();
-                    //System.out.println(teamaux.get().getName());
-                }
-            }
-        }
-        String[] parts = date.split("-");
 
+        tournamentRepository.save(tournament); //save new tournament in tournament table
 
-        Match match = new Match(false, LocalDate.of(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]) , Integer.parseInt(parts[2])), team1,team2);
-
-        matchRepository.save(match);
-        team1.addMatch(match);
-        team2.addMatch(match);
-        teamRepository.save(team1);
-        teamRepository.save(team2);
-        //Optional<Tournament> tournament = tournamentRepository.findByName(torneoSeleccionado);
-        List<Match> matches = new ArrayList<>();
-        matches.add(match);
-        tournament.get().setMatches(matches);*/
-        tournamentRepository.save(tournament); //save the new match in tournament table
-
-        //System.out.println(team.toString());
-        //Match match1 = new Match(date,t1,t2);
-        //List<Match> matches = (List<Match>) match;
-        // tournament.setMatches(matches);
-        //  tournamentRepository.save(tournament);
-        //matchRepository.save(match);
-        /*User u = uc.findByName(user.getName());
-        if (u == null) {
-            uc.save(user);
-            //notificationService.sendNotification(user);
-            //updateTabs(model);
-            return "/signupPlayer";
-        }
-
-       // return "404";*/
         return "/adminPage";
     }
 
     @PostMapping("/saveDataMatch")
-    public String saveDatamatch(String matchSelect,String accuracy1, String effectiveness1, String games_wins1, String unforcedErrors1, String set1team1, String set2team1, String set3team1, String win1, String accuracy2, String effectiveness2, String games_wins2, String unforcedErrors2, String set1team2, String set2team2, String set3team2, String win2) {
+    public String saveDatamatch(String matchSelect, String accuracy1, String effectiveness1, String games_wins1, String unforcedErrors1, String set1team1, String set2team1, String set3team1, String win1, String accuracy2, String effectiveness2, String games_wins2, String unforcedErrors2, String set1team2, String set2team2, String set3team2, String win2) {
         String[] match = matchSelect.split(",");
         Optional<Team> team1 = teamRepository.findByName(match[1]);
         Optional<Team> team2 = teamRepository.findByName(match[2]);
         Optional<Tournament> tournament = tournamentRepository.findByName(match[3]);
-       /* if(team1.isPresent()){
-            System.out.println(team1.get().getName());
-        }else{
-            System.out.println("NO PRESENT");
-        }*/
+
         Match matchDatabase = findMatchByTeams(team1.get(), team2.get(), tournament.get());
-        //System.out.println(matchDatabase.toString());
-        if(matchDatabase != null){
+        if (matchDatabase != null) {
             matchDatabase.setPlayed(true);
 
             int auxAccuracy = Integer.parseInt(accuracy1);
@@ -246,15 +157,12 @@ public class AdminController {
             SetPadel secondSet = new SetPadel(auxSet2, 2);
             auxSets1.add(firstSet);
             auxSets1.add(secondSet);
-            //setPadelRepository.save(firstSet);
-            //setPadelRepository.save(secondSet);
-            if (auxSet3 != -1){
+            if (auxSet3 != -1) {
                 SetPadel thirdSet = new SetPadel(auxSet3, 3);
                 auxSets1.add(thirdSet);
-                //setPadelRepository.save(thirdSet);
             }
             MatchStadistics statsOne = new MatchStadistics(auxSets1, auxAccuracy, auxEffect, auxGamesWin, auxUnforcedError, auxWin);
-            System.out.println("MATCH STADISTICS 1:"+statsOne.toString());
+            System.out.println("MATCH STADISTICS 1:" + statsOne.toString());
             auxAccuracy = Integer.parseInt(accuracy2);
             auxEffect = Integer.parseInt(effectiveness2);
             auxGamesWin = Integer.parseInt(games_wins2);
@@ -268,77 +176,25 @@ public class AdminController {
             SetPadel secondSet2 = new SetPadel(auxSet2, 2);
             auxSets2.add(firstSet2);
             auxSets2.add(secondSet2);
-            //setPadelRepository.save(firstSet);
-            //setPadelRepository.save(secondSet);
 
-            if (auxSet3 != -1){
+            if (auxSet3 != -1) {
                 SetPadel thirdSet2 = new SetPadel(auxSet3, 3);
                 auxSets2.add(thirdSet2);
-                //setPadelRepository.save(thirdSet2);
             }
             MatchStadistics statsTwo = new MatchStadistics(auxSets2, auxAccuracy, auxEffect, auxGamesWin, auxUnforcedError, auxWin);
 
             matchDatabase.setStadistics_1(statsOne);
             matchDatabase.setStadistics_2(statsTwo);
-//            matchStadisticsRepository.save(statsOne);
-  //          matchStadisticsRepository.save(statsTwo);
             matchRepository.save(matchDatabase);
         }
 
-        /*System.out.println("MATCH DATE:"+matchSelect);
-         String[] parts = matchSelect.split("-");
-        Date date = new Date(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]) , Integer.parseInt(parts[2]));
-        System.out.println(Integer.parseInt(parts[0]));
-        System.out.println(Integer.parseInt(parts[1]));
-        System.out.println(Integer.parseInt(parts[2]));
-        Optional<Match> match = matchRepository.findByDate(new Date(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]) , Integer.parseInt(parts[2])));
-        if(match.isPresent()){
-            System.out.println(match.get().toString());
-        }else{
-            System.out.println("NOT PRESENT");
-        }
-
-        //match.get().setScore(score);
-        //matchRepository.save(match.get().getMatch());
-
-        /*System.out.println(torneoSeleccionado);
-        System.out.println(t1);
-        System.out.println(t2);
-        System.out.println("MATCH DATE:"+date);
-        Optional<Team> team = teamRepository.findByName(t1);
-        Optional<Team> team2 = teamRepository.findByName(t2);
-        String[] parts = date.split("-");
-
-        Match match = new Match(new Date(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]) , Integer.parseInt(parts[2])), team.get().getTeam(),team2.get().getTeam());
-        matchRepository.save(match);
-        Optional<Tournament> tournament = tournamentRepository.findByName(torneoSeleccionado);
-        List<Match> matches = new ArrayList<>();
-        matches.add(match);
-        tournament.get().setMatches(matches);
-        tournamentRepository.save(tournament.get()); //save the new match in tournament table
-
-        System.out.println(team.toString());
-        //Match match1 = new Match(date,t1,t2);
-        //List<Match> matches = (List<Match>) match;
-        // tournament.setMatches(matches);
-        //  tournamentRepository.save(tournament);
-        //matchRepository.save(match);
-        /*User u = uc.findByName(user.getName());
-        if (u == null) {
-            uc.save(user);
-            //notificationService.sendNotification(user);
-            //updateTabs(model);
-            return "/signupPlayer";
-        }
-
-       // return "404";*/
         return "/adminPage";
     }
 
-    public Match findMatchByTeams(Team teamOne, Team teamTwo, Tournament tournament){
+    public Match findMatchByTeams(Team teamOne, Team teamTwo, Tournament tournament) {
         List<Match> matches = tournament.getMatches();
         //System.out.println(matches.toString());
-        for(Match m: matches) {
+        for (Match m : matches) {
             //System.out.println("siguente match");
             //System.out.println(m.toString());
             if (!m.isPlayed() && m.hasTeam(teamOne) && m.hasTeam(teamTwo)) {
