124ca0d258fe6fbb2a60c20d7f9c3454205c4d5c
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-02-20 18:19:37 +0100

    TeamxController refactored into TeamService and working

---
+++ src/main/java/com/example/padelversus/team/TeamService.java
@@ -0,0 +1,51 @@
+package com.example.padelversus.team;
+
+import com.example.padelversus.ImageService;
+import com.example.padelversus.player.Player;
+import com.example.padelversus.team.display.TeamxDisplay;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class TeamService {
+
+    @Autowired
+    TeamRepository teamRepository;
+    @Autowired
+    private ImageService imageService;
+
+    public Optional<Team> getTeam(Long id){
+        Optional<Team> team = teamRepository.findByid(id);
+        return team;
+    }
+
+    public TeamxDisplay createTeamxDisplay(Team team){
+        TeamxDisplay teamxDisplay = new TeamxDisplay(team);
+        loadPlayerImages(team);
+        return teamxDisplay;
+    }
+
+    public void loadPlayerImages(Team team){
+        try {
+            Player player1  = team.getMemberN(1);
+            Player player2 = team.getMemberN(2);
+            BufferedImage playerImage1 = player1.getBufferedImage();
+            String base_url = "/images_temp/Player/";
+            String image_name = imageService.saveImage("Player", player1.getId(), playerImage1);
+            String image1_url = base_url + image_name;
+            player1.setImageUrl(image1_url);
+
+            BufferedImage playerImage2 = player2.getBufferedImage();
+            image_name = imageService.saveImage("Player", player2.getId(), playerImage2);
+            String image2_url = base_url + image_name;
+            player2.setImageUrl(image2_url);
+        }catch(IOException e){
+            System.out.println("Fatal error loading player images");
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/padelversus/team/TeamxController.java
+++ src/main/java/com/example/padelversus/team/TeamxController.java
@@ -18,31 +18,18 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/teamx")
 public class  TeamxController {
-
     @Autowired
     TeamRepository teamRepository;
     @Autowired
     private ImageService imageService;
+    @Autowired
+    private TeamService teamService;
 
     @GetMapping("/{id}")
     public String teamx(Model model, @PathVariable Long id) throws IOException {
-        Optional<Team> team = teamRepository.findByid(id);
+        Optional<Team> team = teamService.getTeam(id);
         if(team.isPresent()){
-
-            TeamxDisplay teamxDisplay = new TeamxDisplay(team.get());
-            Player player1  = team.get().getMemberN(1);
-            Player player2 = team.get().getMemberN(2);
-            BufferedImage playerImage1 = player1.getBufferedImage();
-            String base_url = "/images_temp/Player/";
-            String image_name = imageService.saveImage("Player", player1.getId(), playerImage1);
-            String image1_url = base_url + image_name;
-            player1.setImageUrl(image1_url);
-
-            BufferedImage playerImage2 = player2.getBufferedImage();
-            image_name = imageService.saveImage("Player", player2.getId(), playerImage2);
-            String image2_url = base_url + image_name;
-            player2.setImageUrl(image2_url);
-            model.addAttribute("imageAA",image1_url);
+            TeamxDisplay teamxDisplay = teamService.createTeamxDisplay(team.get());
             model.addAttribute("teamInfo", teamxDisplay);
             return "teamx";
         }else{
