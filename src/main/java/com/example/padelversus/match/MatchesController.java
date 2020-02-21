fdbef006989a3e600224493017a7e8752d2a1d69
i100van <i100van@hotmail.com>
2020-02-21 19:34:57 +0100

    findMatchByDateAndPlayedOrderByDate and findAllDates native queries

--- src/main/java/com/example/padelversus/match/MatchesController.java
+++ src/main/java/com/example/padelversus/match/MatchesController.java
@@ -1,54 +1,49 @@
 package com.example.padelversus.match;
 
 import com.example.padelversus.match.display.MatchesByDateDisplay;
-import com.example.padelversus.team.Team;
 import com.example.padelversus.team.display.LastMatchDisplay;
 import com.example.padelversus.tournament.Tournament;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/matches")
 public class MatchesController {
 
-        @Autowired
-        private MatchService matchService;
+    @Autowired
+    private MatchService matchService;
 
-        @GetMapping("/")
-        public String matches(Model model){
-            List<LastMatchDisplay> lastMatches = matchService.lastMatches();
-            List<LastMatchDisplay> nextMatches = matchService.nextMatches();
+    @GetMapping("/")
+    public String matches(Model model) {
+        List<LastMatchDisplay> lastMatches = matchService.lastMatches();
+        List<LastMatchDisplay> nextMatches = matchService.nextMatches();
 
-            List<Match> lastMatchesTournament = matchService.getFourLastMatches();
-            List<Match> nextMatchesTournament = matchService.getFourNextMatches();
-
-
-            List<Tournament> tournamentsNext = matchService.findTournamentsOfMatches(nextMatchesTournament);
-            List<Tournament> tournamentsLast = matchService.findTournamentsOfMatches(lastMatchesTournament);
-            matchService.addNameTournamentOfMatches(lastMatches,tournamentsLast);
-            matchService.addNameTournamentOfMatches(nextMatches,tournamentsNext);
-
-            List<LocalDate> dates = matchService.datesMatchesNextMatchDisplays();
-
-            model.addAttribute("last_matches", lastMatches);
-            model.addAttribute("next_matches",nextMatches);
-
-            List<MatchesByDateDisplay> matchesByDateDisplays = matchService.formMatchesByDateDisplays();
-            model.addAttribute("list_matches_byDate",matchesByDateDisplays);
+        List<Match> lastMatchesTournament = matchService.getFourLastMatches();
+        List<Match> nextMatchesTournament = matchService.getFourNextMatches();
 
 
+        List<Tournament> tournamentsNext = matchService.findTournamentsOfMatches(nextMatchesTournament);
+        List<Tournament> tournamentsLast = matchService.findTournamentsOfMatches(lastMatchesTournament);
+        matchService.addNameTournamentOfMatches(lastMatches, tournamentsLast);
+        matchService.addNameTournamentOfMatches(nextMatches, tournamentsNext);
 
-            return "matches";
+        model.addAttribute("last_matches", lastMatches);
+        model.addAttribute("next_matches", nextMatches);
 
-        }
+        List<MatchesByDateDisplay> lastMatchDisplays = matchService.getMatchesByDateDisplays();
+        model.addAttribute("list_matches_byDate", lastMatchDisplays);
+
+        LocalDate firstDate = matchService.getFirstDate();
+        model.addAttribute("first_date", firstDate);
+
+        return "matches";
+
+    }
 
 }
