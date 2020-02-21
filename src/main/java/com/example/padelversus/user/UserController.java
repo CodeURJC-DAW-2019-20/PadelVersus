3d966077bb4e991e740f618cebc07073770f181a
Lucas Gómez Torres <45000089+LucasGomezTorres@users.noreply.github.com>
2020-02-21 12:23:23 +0100

    minor translations

--- src/main/java/com/example/padelversus/user/UserController.java
+++ src/main/java/com/example/padelversus/user/UserController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Optional;
+
 
 @Controller
 public class UserController {
