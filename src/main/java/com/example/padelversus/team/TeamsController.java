478eadf3ece4ba6f1ddf511bce2926fa62775379
Lucas Gómez Torres <45000089+LucasGomezTorres@users.noreply.github.com>
2020-02-21 11:29:38 +0100

    minor translations

--- src/main/java/com/example/padelversus/team/TeamsController.java
+++ src/main/java/com/example/padelversus/team/TeamsController.java
@@ -1,21 +1,18 @@
 package com.example.padelversus.team;
 
 
-import com.example.padelversus.team.display.TPTournamentDisplayInfo;
+
 import com.example.padelversus.team.display.TeamsPageDisplayInfo;
 import com.example.padelversus.tournament.Tournament;
 import com.example.padelversus.tournament.TournamentRepository;
-import com.example.padelversus.tournament.TournamentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
