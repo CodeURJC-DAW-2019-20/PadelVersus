93a7b42f8eaa09e645e0e6ecfb6782ba8a139af6
unknown <d.carmonap97@gmail.com>
2020-02-22 12:59:47 +0100

    Name of jpg now shows in editprofile

---
+++ src/main/resources/static/js/inputfile.js
@@ -0,0 +1,16 @@
+;( function ( document, window, index ) {
+    var inputs = document.querySelectorAll('.inputfile');
+    Array.prototype.forEach.call(inputs, function (input) {
+        var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener('change', function (e) {
+            var fileName = '';
+            fileName = e.target.value.split('\\').pop();
+            if (fileName)
+                label.querySelector('span').innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+    });
+}(document, window, 0));
