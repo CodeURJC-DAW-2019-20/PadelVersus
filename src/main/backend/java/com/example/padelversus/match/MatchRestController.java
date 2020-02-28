2ec87017bec749bbc47a36ab58337eb7a2a64967
AlexCh98 <alexcheca.98@gmail.com>
2020-02-28 14:49:42 +0100

    saveStatsMatch working

--- src/main/backend/java/com/example/padelversus/match/MatchRestController.java
+++ src/main/backend/java/com/example/padelversus/match/MatchRestController.java
@@ -1,8 +1,11 @@
 package com.example.padelversus.match;
 
 import com.example.padelversus.match.stadistics.MatchStadistics;
+import com.example.padelversus.match.stadistics.SetPadel;
 import com.example.padelversus.team.Team;
+import com.example.padelversus.team.TeamService;
 import com.example.padelversus.tournament.Tournament;
+import com.example.padelversus.tournament.TournamentService;
 import com.fasterxml.jackson.annotation.JsonView;
 import jdk.internal.instrumentation.TypeMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +22,16 @@ import java.util.function.ToDoubleBiFunction;
 @RestController
 @RequestMapping("/api")
 public class MatchRestController {
-    interface CompleteInfoForAMatch extends Match.Basic,Match.Statistics, MatchStadistics.Basic, Match.Teams,
+    interface CompleteInfoForAMatch extends Match.Basic,Match.Statistics, MatchStadistics.Basic, Match.Teams,MatchStadistics.SetP, SetPadel.Basic,
             Team.Basic{}
 
     @Autowired
     MatchService matchService;
 
+    @Autowired
+    TeamService teamService;
+    @Autowired
+    TournamentService tournamentService;
     @JsonView(MatchRestController.CompleteInfoForAMatch.class)
     @GetMapping("/match/{id}")
     public ResponseEntity<Match> getMatchById(@PathVariable Long id) {
@@ -71,30 +78,31 @@ public class MatchRestController {
 
     @JsonView(MatchRestController.CompleteInfoForAMatch.class)
     @RequestMapping(value = "/match/{tournament}",method = RequestMethod.POST)
-    public ResponseEntity<Match> saveMatchMatch(@RequestParam Tournament tournament, @RequestBody Match match){
-        Team team1 = match.getTeams().get(0);
-        Team team2 = match.getTeams().get(1);
-        Match optionalMatch = matchService.findMatchByTeams(team1,team2,tournament);
+    public ResponseEntity<Match> saveMatchMatch(@PathVariable String tournament, @RequestBody MatchCreate match){
+        Optional<Tournament> tournament1 = tournamentService.getTournamentByName(tournament);
+        Team team1 = teamService.getTeamByName(match.t1).get();
+        Team team2 = teamService.getTeamByName(match.t2).get();
+
+        Match optionalMatch = matchService.findMatchByTeams(team1,team2,tournament1.get());
         if(optionalMatch!=null) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
         Match matchOf = new Match(false,match.getDate(),team1,team2);
-        matchService.saveApiMatch(matchOf,tournament);
-        Match matchOficial = matchService.findMatchByTeams(team1,team2,tournament);
+        matchService.saveApiMatch(matchOf,tournament1.get());
+        Match matchOficial = matchService.findMatchByTeams(team1,team2,tournament1.get());
         return new ResponseEntity<>(matchOficial,HttpStatus.CREATED);
     }
+
     @JsonView(MatchRestController.CompleteInfoForAMatch.class)
-    @RequestMapping(value = "/match/{tournament}",method = RequestMethod.PUT)
-    public ResponseEntity<Match> saveStatsMatch(@RequestParam Tournament tournament, @RequestBody Match match){
-        Team team1 = match.getTeams().get(0);
-        Team team2 = match.getTeams().get(1);
-        Match optionalMatch = matchService.findMatchByTeams(team1,team2,tournament);
-        if(optionalMatch!=null) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        Match matchOf = new Match(false,match.getDate(),team1,team2);
-        matchService.saveApiMatch(matchOf,tournament);
-        Match matchOficial = matchService.findMatchByTeams(team1,team2,tournament);
-        return new ResponseEntity<>(matchOficial,HttpStatus.CREATED);
+    @RequestMapping(value = "/match/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<Match> saveStatsMatch(@PathVariable Long id, @RequestBody List<MatchStadistics> matchStadistics){
+        Optional<Match> match = matchService.findMatchById(id);
+        Match matchOficial = match.get();
+        matchOficial.setStadistics_1(matchStadistics.get(0));
+        matchOficial.setStadistics_2(matchStadistics.get(1));
+        matchOficial.getTeams().get(0).updateTeamStatistics(matchStadistics.get(0));
+        matchOficial.getTeams().get(1).updateTeamStatistics(matchStadistics.get(1));
+        matchService.saveMatch(matchOficial);
+        return new ResponseEntity<>(matchOficial,HttpStatus.OK);
     }
 }
