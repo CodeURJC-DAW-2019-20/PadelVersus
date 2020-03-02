65eb7b2c582ddaa0c6dd5065a814f0bf4dbf9b1c
i100van <i100van@hotmail.com>
2020-03-02 18:55:48 +0100

    Upload docker files working

---
+++ Script.ps1
@@ -0,0 +1,9 @@
+docker run -it --rm -v "$(pwd):/usr/src/project" -w /usr/src/project maven:alpine mvn package
+Copy-Item -Path ./target/padelversus-0.0.1-SNAPSHOT.jar -Destination ./Docker/PadelVersus.jar -force
+Copy-Item -Path ./DemoImages -Destination ./Docker/DemoImages -Recurse
+cd Docker
+docker rmi padelversus
+docker image build -t padelversus -f .Dockerfile .
+docker-compose up -d
+cd ..
+
