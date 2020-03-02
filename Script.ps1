d54be9aa8da3076eee7d8f4d3ecfb2fb72cef0eb
i100van <32304155+i100van@users.noreply.github.com>
2020-03-02 18:58:15 +0100

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -1,4 +1,4 @@
-docker run -it --rm -v "$(pwd):/usr/src/project" -w /usr/src/project maven:alpine mvn package
+docker run -it --rm -v "$(pwd):/usr/src/project" -w /usr/src/project maven:alpine mvn clean install
 Copy-Item -Path ./target/padelversus-0.0.1-SNAPSHOT.jar -Destination ./Docker/PadelVersus.jar -force
 Copy-Item -Path ./DemoImages -Destination ./Docker/DemoImages -Recurse
 cd Docker
