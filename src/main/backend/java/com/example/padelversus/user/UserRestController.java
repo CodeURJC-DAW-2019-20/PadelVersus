eec5492057e301a3135d70cf5f08029e864dcedd
AlexCh98 <alexcheca.98@gmail.com>
2020-03-02 11:52:10 +0100

    save new user working

--- src/main/backend/java/com/example/padelversus/user/UserRestController.java
+++ src/main/backend/java/com/example/padelversus/user/UserRestController.java
@@ -3,12 +3,9 @@ package com.example.padelversus.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
 
 @RestController
@@ -30,6 +27,21 @@ public class UserRestController{
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
+    }
+
+    @PostMapping(value = "/saveUser" )
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        Optional<User> optionalUser = userService.findUserByName(user.getName());
+        if(optionalUser.isPresent()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        userService.saveUser(user);
+        Optional<User> user1 = userService.findUserByName(user.getName());
+        if(user1.isPresent()){
+            return new ResponseEntity<>(user1.get(), HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 
 }
