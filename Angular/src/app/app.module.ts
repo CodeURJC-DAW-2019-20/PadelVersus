a78669e07dab4af60e2083ad951bfcf032ce0e5e
i100van <i100van@hotmail.com>
2020-03-11 19:51:41 +0100

    Hotfix duplicated imports

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -11,14 +11,6 @@ import {MatGridListModule} from "@angular/material/grid-list";
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
 import {MatCarouselModule} from '@ngmodule/material-carousel';
 
 
