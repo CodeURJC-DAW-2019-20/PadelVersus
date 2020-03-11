79aa302cc8d61a52f0ceb1c33947d4bc400a22c7
i100van <i100van@hotmail.com>
2020-03-11 19:30:23 +0100

    working games count

--- Angular/src/app/home/home.component.ts
+++ Angular/src/app/home/home.component.ts
@@ -26,6 +26,7 @@ export class HomeComponent implements OnInit {
     return this.nextMatches;
   }
 
+
   private handleError(error: any) {
     console.error(error);
   }
--- Angular/src/app/match/match.component.ts
+++ Angular/src/app/match/match.component.ts
@@ -23,6 +23,25 @@ export class MatchComponent implements  OnInit{
   getMatch(){
     return this.matchInfo;
   }
+  getResult(){
+    let nsets = 0;
+    let score = [];
+    score[0]=0
+    score[1]=0
+    for (let i = 0; i < this.matchInfo.stadistics_1.sets.length; i++) {
+      nsets+=1
+      let game_per_set_t1 = this.matchInfo.stadistics_1.sets[i].games
+      let game_per_set_t2 = this.matchInfo.stadistics_2.sets[i].games
+      if (game_per_set_t1 > game_per_set_t2) {
+        score[1] += 1;
+      } else {
+        score[2] += 1;
+      }
+    }
+    console.log(score.toString())
+    return score;
+
+  }
 
   private handleError(error: any) {
     console.error(error);
