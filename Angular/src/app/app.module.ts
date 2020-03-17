21df56ff0d38d9ecabb1a509bcb824ad9f7f7576
AlexCh98 <alexcheca.98@gmail.com>
2020-03-17 11:36:06 +0100

    Log In working

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,23 +1,39 @@
 
-
-import {MatCardModule} from '@angular/material/card';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import {MatchComponent} from './match/match.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {MatGridListModule} from "@angular/material/grid-list";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {HttpClientJsonpModule} from "@angular/common/http";
+
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCarouselModule} from '@ngmodule/material-carousel';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+
+import {NgApexchartsModule} from 'ng-apexcharts';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {PlayerComponent} from './player/player.component';
+import {TournamentComponent} from './tournament/tournament.component';
+import {TournamentRankingComponent} from './tournament/tournament-ranking/tournament-ranking.component';
+import {MatchComponent} from './match/match.component';
+import {NextMatchesComponent} from './home/next-matches/next-matches.component';
+import {LastMatchesComponent} from './home/last-matches/last-matches.component';
+import {CarouselComponent} from './home/carousel/carousel.component';
+import {MatchesComponent} from './matches/matches.component';
+import {NextMatchesDateComponent } from './matches/next-matches-date/next-matches-date.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import {NotFoundComponent} from './not-found/not-found.component';
+import { ImagesComponent } from './images/images.component';
+
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {HttpClientJsonpModule} from "@angular/common/http";
 import {AdminComponent} from "./admin/admin.component";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SignUpComponent} from "./signUp/signUp.component";
 import {LoginComponent} from "./logIn/login.component";
 import {AuthenticationService} from "./authentication.service";
@@ -34,29 +50,38 @@ import {ErrorInterceptor} from "./error.interceptor";
   declarations: [
     AppComponent,
     HomeComponent,
+    TournamentComponent,
+    TournamentRankingComponent,
     MatchComponent,
+    NextMatchesComponent,
+    LastMatchesComponent,
+    CarouselComponent,
+    MatchesComponent,
+    NextMatchesDateComponent,
+    NotFoundComponent,
+    PlayerComponent,
+    ImagesComponent,
     AdminComponent,
     SignUpComponent,
     LoginComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    HttpClientJsonpModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatCardModule,
-
     MatProgressBarModule,
     MatGridListModule,
-
-    MatGridListModule,
+    MatButtonModule,
+    MatTabsModule,
     MatCarouselModule,
+    NgApexchartsModule,
+    FormsModule,
+    HttpClientJsonpModule,
     MatFormFieldModule,
     MatSelectModule,
-    FormsModule,
     ReactiveFormsModule,
-
     FlexLayoutModule,
     MatCheckboxModule,
     MatIconModule,
