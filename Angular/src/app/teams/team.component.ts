8fb8d0511ddfc63fa5cbfca23dafd9a07536ae94
unknown <d.carmonap97@gmail.com>
2020-03-20 12:40:19 +0100

    Add chart code in team

--- Angular/src/app/teams/team.component.ts
+++ Angular/src/app/teams/team.component.ts
@@ -6,16 +6,48 @@ import {ActivatedRoute} from "@angular/router";
 import {Match} from "../Interfaces/match.model";
 import {MatchesService} from "../matches/matches.service";
 
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexFill,
+  ApexMarkers,
+  ApexYAxis
+} from "ng-apexcharts";
+
+export interface ChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  fill: ApexFill;
+  markers: ApexMarkers;
+  yaxis: ApexYAxis;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+}
+
 @Component({
   selector: 'app-teams',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css'],
 })
+
+
 export class TeamComponent implements OnInit {
 
   private id: number;
   private team: Team;
   private lastMatches: Match[];
+
+  @ViewChild('chart') chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
   constructor(private activatedRoute: ActivatedRoute,
               private teamService: TeamService,
@@ -52,4 +84,81 @@ export class TeamComponent implements OnInit {
     return this.lastMatches
   }
 
+  setChartOptions(): void {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Likes",
+          data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line"
+      },
+      stroke: {
+        width: 7,
+        curve: "smooth"
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "1/11/2000",
+          "2/11/2000",
+          "3/11/2000",
+          "4/11/2000",
+          "5/11/2000",
+          "6/11/2000",
+          "7/11/2000",
+          "8/11/2000",
+          "9/11/2000",
+          "10/11/2000",
+          "11/11/2000",
+          "12/11/2000",
+          "1/11/2001",
+          "2/11/2001",
+          "3/11/2001",
+          "4/11/2001",
+          "5/11/2001",
+          "6/11/2001"
+        ]
+      },
+      title: {
+        text: "Social Media",
+        align: "left",
+        style: {
+          fontSize: "16px",
+          color: "#666"
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "dark",
+          gradientToColors: ["#FDD835"],
+          shadeIntensity: 1,
+          type: "horizontal",
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100, 100, 100]
+        }
+      },
+      markers: {
+        size: 4,
+        colors: ["#FFA41B"],
+        strokeColors: "#fff",
+        strokeWidth: 2,
+        hover: {
+          size: 7
+        }
+      },
+      yaxis: {
+        min: -10,
+        max: 40,
+        title: {
+          text: "Engagement"
+        }
+      }
+    };
+  }
 }
