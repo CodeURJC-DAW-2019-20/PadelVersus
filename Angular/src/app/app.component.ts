96eedafa87f1e3bb435e4adc7d9aa9ea7b673c26
Jose Luis <jllavado0@gmail.com>
2020-04-04 11:44:42 +0200

    Footer hiding

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
+  public headerFooter: boolean;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          const noFooteerRoutes = [
+            '/login',
+            '/signup',
+            '/signupplayer',
+            '/logout',
+            '/404',
+            '/403'
+          ];
+          this.headerFooter = noFooteerRoutes.indexOf(event.url) === -1;
+          console.error(event.url);
+        }
+      });
+  }
 }
