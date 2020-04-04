34fec8b59dc518d5ab6bedd6911441b23e9f1003
Jose Luis <jllavado0@gmail.com>
2020-04-04 11:52:20 +0200

    admin redirect to /admin if try to register into tournament

--- Angular/src/app/auth.guard.ts
+++ Angular/src/app/auth.guard.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (localStorage.getItem('currentUser')) {
+      let nameStored = localStorage.getItem('currentUser').split(',')[1].split(':')[1];
+      nameStored = nameStored.substr(1, nameStored.length - 2);
+      if (nameStored === 'admin') {
+        // logged in as admin so go /admin
+        this.router.navigate(['/admin']);
+      }
       // logged in so return true
       return true;
+
     }
 
     // not logged in so redirect to login page with the return url
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
     return false;
   }
 }
