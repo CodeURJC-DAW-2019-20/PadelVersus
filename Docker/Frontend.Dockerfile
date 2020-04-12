f3742f4b547087da997553161ea911b016513a6a
Jose Luis <jllavado0@gmail.com>
2020-04-12 13:52:37 +0200

    Dockerfile and script

--- Docker/Frontend.Dockerfile
+++ Docker/Frontend.Dockerfile
@@ -1,11 +1,6 @@
 FROM node:12.16.2-alpine
 
 WORKDIR /usr/src/app
-# Acordarse de antes de copiar package.json y package-lock.json a la carpeta docker
-COPY ../Docker ./
 
-RUN npm install
-RUN npm install -g @angular/cli@9.0.7
-
-CMD ls -la .
+CMD npm install && npm run build
 
--- Script.ps1
+++ Script.ps1
@@ -1,8 +1,25 @@
 # Line to run from standard cmd like intellij idea one (only windows)
 # Execution without log
 # C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -File D:\EntornoDesarrollo\DAW\PadelVersus\Script.ps1
-# Execution with log
+# Execution with log41
 # C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -File C:/Users/jllav/IdeaProjects/PadelVersus/Script.ps1 $true
+cd Docker
+docker rmi teampina/node-js
+docker image build -t teampina/node-js -f Frontend.Dockerfile .
+cd ..
+
+cd Angular
+docker run  -v "$(pwd):/usr/src/app" teampina/node-js
+cd ..
+
+if (!(Test-Path ./Backend/src/main/resources/static/new -PathType Any)){
+    Copy-Item -Path ./Angular/dist/angular -Destination ./Backend/src/main/resources/static/new -Recurse
+}else{
+    Remove-Item -Recurse -Force ./Backend/src/main/resources/static/new
+    Copy-Item -Path ./Angular/dist/angular -Destination ./Backend/src/main/resources/static/new -Recurse
+}
+Remove-Item -Recurse -Force ./Angular/dist
+
 cd Backend
 # Clean and packge using local .m2 repository to do not download already gotten libraries
 docker run -it --rm -v "$(pwd):/usr/src/project" `
@@ -17,6 +34,7 @@ if(Test-Path ./target/padelversus-0.0.1-SNAPSHOT.jar -PathType Leaf){
     exit
 }
 cd ..
+
 # If there is not already moved we copy recursive the folder with images needed for the demo
 if (!(Test-Path ./Docker/DemoImages -PathType Any)){
     Copy-Item -Path ./DemoImages -Destination ./Docker/DemoImages -Recurse
@@ -28,14 +46,14 @@ if (!(Test-Path ./Docker/DemoImages -PathType Any)){
 # We move into the folder were the .Dockerfile and docker-compose.yml is
 cd Docker
 
-# We remove the image in case we have done changes in .Dockerfile
+# We remove the images in case we have done changes in .Dockerfile
 docker rmi teampina/padelversus
 
-# We build the app image
-docker image build -t teampina/padelversus -f .Dockerfile .
+# We build the backend image
+docker image build -t teampina/padelversus -f Backend.Dockerfile .
 
-# We push the image to docker hub
-# cd docker push teampina/padelversus
-
+# We push the images to docker hub
+push teampina/padelversus
+push teampina/node-js
 # We reutrned to the started directory
 cd ..
